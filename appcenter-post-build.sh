057be7f490c675135b01df4102dedd6f97801533
Vlad Tomsa <tomsavlad90@gmail.com>
2021-06-08 19:41:38 +0300

    edit post build script - add logging env variables 5

--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -6,11 +6,11 @@ echo "Run Unit Tests"
 echo "**************************************************************************************************"
 echo "Working directory:" $APPCENTER_SOURCE_DIRECTORY
 echo "Output directory:" $APPCENTER_OUTPUT_DIRECTORY
+echo "App Center API TOKEN: " $APP_CENTER_API_TOKEN
 echo "**************************************************************************************************"
 echo 
-
 ls
-
+echo "Changing directory"
 cd $APPCENTER_OUTPUT_DIRECTORY
-
+echo "Inside changed directory"
 ls
