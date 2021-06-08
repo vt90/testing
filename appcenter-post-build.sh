0b74b53ea18ea3e5dcc4bc6afadd4764caf29fdd
Vlad Tomsa <tomsavlad90@gmail.com>
2021-06-08 23:22:03 +0300

    edit post build script - add logging env variables 11

--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -14,11 +14,26 @@ echo
 echo
  xcodebuild -version
 
+
+
+rm -rf DerivedData2
+
+xcrun xcodebuild build-for-testing \
+  -configuration Debug \
+  -project testing.xcodeproj \
+  -sdk iphoneos \
+  -scheme  testing \
+  -derivedDataPath DerivedData2
+
+
+ls
+
+
 appcenter login --token $APP_CENTER_API_TOKEN
 echo "Successfully logged in"
-appcenter test run xcuitest \
-  --app "tomsavlad90/Hello" \
-  --devices "tomsavlad90/ios-devices" \
-  --test-series “master” \
-  --locale "en_US" \
-  --build-dir DerivedData/Build/Products/Debug-iphoneos 
+#appcenter test run xcuitest \
+#  --app "tomsavlad90/Hello" \
+#  --devices "tomsavlad90/ios-devices" \
+#  --test-series “master” \
+#  --locale "en_US" \
+#  --build-dir DerivedData/Build/Products/Debug-iphoneos 
