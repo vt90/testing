737c839728651ae1a4008d4e05c4b6715bda0ada
Vlad Tomsa <tomsavlad90@gmail.com>
2021-06-08 19:56:52 +0300

    edit post build script - add logging env variables 6

--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -10,7 +10,14 @@ echo "App Center API TOKEN: " $APP_CENTER_API_TOKEN
 echo "**************************************************************************************************"
 echo 
 ls
-echo "Changing directory"
-cd $APPCENTER_OUTPUT_DIRECTORY
-echo "Inside changed directory"
-ls
+echo
+echo
+    echo "> UI test command to run:"
+    echo "appcenter test run uitest" 
+    echo "--app $appName" 
+    echo "--devices tomsavlad90/ios-devices"
+    echo "--app-path $APPCENTER_OUTPUT_DIRECTORY/*.ipa"
+    echo "--test-series master"
+    echo "--locale \"en_US\"" 
+    echo "--build-dir $APPCENTER_SOURCE_DIRECTORY/DerivedData/Build/Products/Debug-iphoneos"
+    echo "--token $APP_CENTER_API_TOKEN"
